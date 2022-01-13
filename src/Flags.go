17aff0d89eb7e96fd11c4df2066c7264143742b5
sstringer <sstringer@gentlemanfox.co.uk>
2022-01-13 11:44:06 +0000

    Updated readme and fixed some typos

--- src/Flags.go
+++ src/Flags.go
@@ -7,7 +7,7 @@ func ProcessFlags() AppConfig {
 	var verbose bool
 	var dryrun bool
 
-	flag.StringVar(&config, "ConfigFile", "config.json", "The location of the configuration file")
+	flag.StringVar(&config, "f", "config.json", "The location of the configuration file")
 	flag.BoolVar(&verbose, "v", false, "Verbose - When true, verbose logging is enabled.")
 	flag.BoolVar(&dryrun, "d", false, "Dry Run - When true, no changes will be made the database/s")
 
