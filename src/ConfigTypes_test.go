f8a21843844023ab7629aaf02f139b47baba300c
sstringer <sstringer@gentlemanfox.co.uk>
2022-03-07 11:03:53 +0000

    Added printconfig flag and functionality

--- src/ConfigTypes_test.go
+++ src/ConfigTypes_test.go
@@ -23,3 +23,22 @@ func TestConfig_CheckForDupeNames(t *testing.T) {
 		})
 	}
 }
+
+func TestConfig_PrintConfig(t *testing.T) {
+	/*Sort of hard to test for faults here as we need valid json to work with in the first place*/
+	tests := []struct {
+		name    string
+		c       *Config
+		wantErr bool
+	}{
+		{"GoodSingleDB", &Config{true, 60, true, 60, true, true, 60, true, true, 60, true, []DbConfig{{"systemdb_TST", "hanadb.mydomain.int", 30015, "hccuser", "ReallyCoolPassw0rd", true, 60, true, 60, true, true, 60, true, true, 60, true, nil}}}, false},
+		{"GoodTwoDBs", &Config{true, 60, true, 60, true, true, 60, true, true, 60, true, []DbConfig{{"systemdb_TST", "hanadb.mydomain.int", 30015, "hccuser", "ReallyCoolPassw0rd", true, 60, true, 60, true, true, 60, true, true, 60, true, nil}, {"ten1_TST", "hanadb.mydomain.int", 30041, "hccuser", "ReallyCoolPassw0rd", false, 0, false, 0, true, true, 90, false, true, 30, true, nil}}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.c.PrintConfig(); (err != nil) != tt.wantErr {
+				t.Errorf("Config.PrintConfig() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
--- src/Flags.go
+++ src/Flags.go
@@ -6,12 +6,14 @@ func ProcessFlags() AppConfig {
 	var config string
 	var verbose bool
 	var dryrun bool
+	var printconfig bool
 
 	flag.StringVar(&config, "f", "config.json", "The location of the configuration file")
 	flag.BoolVar(&verbose, "v", false, "Verbose - When true, verbose logging is enabled.")
 	flag.BoolVar(&dryrun, "d", false, "Dry Run - When true, no changes will be made the database/s")
+	flag.BoolVar(&printconfig, "p", false, "Print Effective Config - When true, the application configuration is printed to screen and the application quits")
 
 	flag.Parse()
 
-	return AppConfig{config, verbose, dryrun}
+	return AppConfig{config, verbose, dryrun, printconfig}
 }
