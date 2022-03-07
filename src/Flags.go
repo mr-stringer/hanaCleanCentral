b2b3fb104e8a45e0914547f2c1de564a9979f2d0
sstringer <sstringer@gentlemanfox.co.uk>
2022-03-07 15:39:05 +0000

    DbConfig.Password changed to unexported for secure printing

--- src/Flags.go
+++ src/Flags.go
@@ -11,7 +11,7 @@ func ProcessFlags() AppConfig {
 	flag.StringVar(&config, "f", "config.json", "The location of the configuration file")
 	flag.BoolVar(&verbose, "v", false, "Verbose - When true, verbose logging is enabled.")
 	flag.BoolVar(&dryrun, "d", false, "Dry Run - When true, no changes will be made the database/s")
-	flag.BoolVar(&printconfig, "p", false, "Print Effective Config - When true, the application configuration is printed to screen and the application quits")
+	flag.BoolVar(&printconfig, "p", false, "Print Effective Config - When true, the application configuration is printed to screen and the application quits\nPasswords will not be printed!")
 
 	flag.Parse()
 
